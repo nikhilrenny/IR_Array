8ebfe503f86be3f46c5ac1911d7f5975f9791857
nikhilrenny <nikhiltomrenny@gmail.com>
2025-11-19 01:07:18 +0000

    Update IR_Array.h

--- src/IR_Array.h
+++ src/IR_Array.h
@@ -2,13 +2,16 @@
 #define IR_ARRAY_H
 
 #include <Arduino.h>
+#include <vector>
 
 class IR_Array {
 public:
     // Constructor
     IR_Array(uint8_t count);
+
     // Setup (pin assignment + K value)
-    void setup(const uint8_t* pins = nullptr, int k = 2);
+    void setup(const std::vector<uint8_t>& pins = {}, int k = 2);
+
     // Main functions
     void CalibrateSensors(uint16_t ctime = 5000);
     void Read(uint8_t mode = 0, uint8_t pd = 0);
@@ -16,21 +19,22 @@ public:
 
 private:
     // Sensor Properties
-    const uint8_t SensorCount;   // Number of sensors
-    uint8_t*  SensorPins;        // Sensor pin array
-    int       K;                 // Sigma factor
-    float     Sum_Std_Dev;
+    const uint8_t SensorCount;       // Number of sensors
+    std::vector<uint8_t> SensorPins; // Sensor pin array
+    int K;                            // Sigma factor
+    float Sum_Std_Dev;
 
     // Statistics Arrays
-    uint16_t* Count;
-    float*    Sum;
-    float*    Sum_Sq;
-    float*    SensorMin;
-    float*    SensorMax;
-    float*    Range;
-    float*    Mean;
-    float*    Variance;
-    float*    Std_Dev;
-    float*    SensorStatus;
+    std::vector<uint16_t> Count;
+    std::vector<float> Sum;
+    std::vector<float> Sum_Sq;
+    std::vector<float> SensorMin;
+    std::vector<float> SensorMax;
+    std::vector<float> Range;
+    std::vector<float> Mean;
+    std::vector<float> Variance;
+    std::vector<float> Std_Dev;
+    std::vector<float> SensorStatus;
 };
+
 #endif
